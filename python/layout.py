ae76c87e52cf07b6b780512d77b89b5cd053aac3
Sam Hartman <hartmans@debian.org>
2021-04-26 11:46:02 -0600

    Fixes:
    * Use the name as the management IP address.
    
    * Include ssh authorized keys file in our image
    
    * Split out user creation from package installation on hadron.algebra
      for better fault tolerance

--- python/layout.py
+++ python/layout.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from carthage.dependency_injection import *
 from carthage import *
 from carthage.modeling import *
@@ -8,12 +9,24 @@ from carthage.machine import *
 from carthage.debian import *
 from carthage.network import V4Config
 
-class OurMachine(MachineModel, SystemdNetworkModelMixin, template = True): pass
+class OurMachine(MachineModel, SystemdNetworkModelMixin, template = True):
+
+    @property
+    def ip_address(self):
+        return self.name #manage through the host name
+
 
 class IaLayout(CarthageLayout):
 
-    
-        
+    @provides(container_image)
+    class OurImage(DebianContainerImage):
+        ssh_authorization = customization_task(SshAuthorizedKeyCustomizations)
+
+    # We don't need an origin
+    add_provider(ssh_origin, None)
+
+
+
     class suchdamage_enclave(Enclave):
 
         domain = "suchdamage.org"
@@ -37,7 +50,7 @@ class IaLayout(CarthageLayout):
                          masquerade = True,
                          ),
                      )
-                
+
 
             class Cust(MachineCustomization):
 
@@ -45,11 +58,11 @@ class IaLayout(CarthageLayout):
                 def enable_resolved(self):
                     host = self.host
                     host.shell("systemctl", "enable", "--now", "systemd-resolved")
-                        
+
 
     class ia_enclave(Enclave):
 
-        
+
         @globally_unique_key("ia_network")
         class ia_net(NetworkModel):
 
@@ -64,20 +77,20 @@ class IaLayout(CarthageLayout):
 
         domain = "algebra"
         add_provider(machine_implementation_key, dependency_quote(Container))
-        add_provider(container_image, DebianContainerImage)
+
 
         class net_config(NetworkConfigModel):
 
             ia_net = injector_access("ia_net")
             add("eth0", net = ia_net, mac = persistent_random_mac,
                 )
-            
+
         class apt(OurMachine):
 
             nginx_config = mako_task("apt_site.mako", output = "etc/nginx/sites-enabled/apt")
 
             container_args = ['--bind=/debian']
-            
+
             class cust(ContainerCustomization):
 
                 install_packages = install_stage1_packages_task(['nginx'])
@@ -100,7 +113,7 @@ class IaLayout(CarthageLayout):
 
             class network_config(NetworkConfigModel):
                 ia_net = injector_access("ia_network")
-                    
+
 
                 add('eth0', mac = persistent_random_mac,
                     net = ia_net,
@@ -110,5 +123,27 @@ class IaLayout(CarthageLayout):
                         dns_servers = ["127.0.0.1"],
                         dhcp = False),
                     )
-                    
-            
+
+
+        class hadron(OurMachine):
+
+            container_args=["--bind=/home/hartmans/hadron:/hadron"]
+
+            class Cust(MachineCustomization):
+
+                @setup_task("make user")
+                async def make_user(self):
+                    await self.ssh("useradd -u 8042 -m hartmans",
+                                   _bg = True,
+                                   _bg_exc = False)
+
+                @setup_task("install packages")
+                async def install_dev_packages(self):
+                    await self.ssh("apt update",
+                               _bg = True, _bg_exc = False)
+                    await self.ssh('apt -y install ansible git emacs-nox python3-sqlalchemy python3-tornado python3-pyvmomi',
+                               _bg = True,
+                                   _bg_exc = False)
+                    async with self.filesystem_access() as path:
+                        home_hartmans = Path(path)/"home/hartmans"
+                    home_hartmans.joinpath("hadron").symlink_to("/hadron")
