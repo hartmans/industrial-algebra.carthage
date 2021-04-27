fcd22ee28d9c2e47bcb874baca8d04ad129bf1da
Sam Hartman <hartmans@debian.org>
2021-04-27 07:31:00 -0600

    Enable hadron ansible
    Enable hadron ansible variables for the layout.

--- carthage_plugin.py
+++ carthage_plugin.py
@@ -1,9 +1,19 @@
 from .layout import IaLayout
 from carthage import *
 import asyncio
+import hadron.carthage.ansible
+import carthage.ansible
 
 import carthage
 
-@inject(injector = Injector)
-def  carthage_plugin(injector):
+@inject(injector = Injector,
+        config = ConfigLayout)
+def  carthage_plugin(injector, config):
     injector.add_provider(layout.IaLayout)
+    injector.add_provider(InjectionKey(carthage.ansible.AnsibleHostPlugin, name = "hadron"),
+                          hadron.carthage.ansible.HadronHostPlugin)
+    injector.add_provider(hadron.inventory.config.generator.ConfigCache)
+    injector.add_provider(hadron.inventory.config.generator.hadron_config_dir_key,
+                          config.output_dir+"/hadron")
+    
+    
