bce92006e524420461bc8344b0882c0a1d378254
Sam Hartman <hartmans@debian.org>
2023-01-31 14:07:56 -0700

    Remove h-o references

--- carthage_plugin.py
+++ carthage_plugin.py
@@ -1,7 +1,6 @@
 from .layout import IaLayout
 from carthage import *
 import asyncio
-import hadron.carthage.ansible
 import carthage.ansible
 
 import carthage
@@ -10,10 +9,5 @@ import carthage
         config = ConfigLayout)
 def  carthage_plugin(injector, config):
     injector.add_provider(layout.IaLayout)
-    injector.add_provider(InjectionKey(carthage.ansible.AnsibleHostPlugin, name = "hadron"),
-                          hadron.carthage.ansible.HadronHostPlugin)
-    injector.add_provider(hadron.inventory.config.generator.ConfigCache)
-    injector.add_provider(hadron.inventory.config.generator.hadron_config_dir_key,
-                          config.output_dir+"/hadron")
     
     
